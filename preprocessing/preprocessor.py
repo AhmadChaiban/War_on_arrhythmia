4875d79d8d1b27e9fdff3ff5cacde2be9e9855a6
Ahmad Chaiban <ahmadchaiban@gmail.com>
2020-06-11 03:15:23 +0300

    Feat (signal creator)
    Created the signal creator function in the preprocessor file.

--- preprocessing/preprocessor.py
+++ preprocessing/preprocessor.py
@@ -1,3 +1,17 @@
+import numpy as np
+
 class preprocessor:
     def __init__(self):
         pass
+
+    def signal_creator(self, raw_signal):
+        signal_data = []
+        for i in range(len(raw_signal[0])):
+            signal_data.append(raw_signal[0][i][0])
+        signal_data = np.array(signal_data)
+        return signal_data
+
+if __name__ == '__main__':
+    prep = preprocessor()
+
+    # prep.signal_creator()
