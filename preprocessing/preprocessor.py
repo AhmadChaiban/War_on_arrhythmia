890edcb2a9912c628342732f4539cf351959c176
Ahmad Chaiban <ahmadchaiban@gmail.com>
2020-06-15 16:53:59 +0300

    Feat (replacing null values with mean)

--- preprocessing/preprocessor.py
+++ preprocessing/preprocessor.py
@@ -11,6 +11,15 @@ class preprocessor:
         signal_data = np.array(signal_data)
         return signal_data
 
+    def clear_null_values(self, signal_data):
+        nulls = np.isnan(signal_data)
+        null_data_indexes = []
+        for i in range(len(nulls)):
+            if nulls[i] == True:
+                null_data_indexes.append(i)
+                signal_data[i] = 0
+            signal_data[i] = np.mean(signal_data)
+
 if __name__ == '__main__':
     prep = preprocessor()
 
